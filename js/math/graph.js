80a9322fa8ec7b105a646c1cf4085474cfa33a93
LeoMcBills <leokinyera9@gmail.com>
2024-01-28 13:24:01 +0300

    auto pushed

--- js/math/graph.js
+++ js/math/graph.js
@@ -12,6 +12,14 @@ class Graph {
         return this.points.find((p) => p.equals(point));
     }
 
+    tryAddPoint(point) {
+        if (!this.containsPoint(point)) {
+            this.addPoint(point);
+            return true;
+        }
+        return false;
+        
+    }
     draw(ctx) {
         for (const seg of this.segments) {
             seg.draw(ctx);
