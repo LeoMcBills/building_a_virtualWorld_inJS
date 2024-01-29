b7cf9f570ec1d2ffd58ddfdf1e96754191fc98a9
LeoMcBills <leokinyera9@gmail.com>
2024-01-29 11:48:09 +0300

    auto pushed

--- prac.js
+++ prac.js
@@ -7,3 +7,11 @@ function add(a, b) {
 }
 
 console.log(add(1, 2))
+
+function subtract(a, b) {
+    return a - b;
+}
+
+let value;
+value = subtract(6, 2);
+console.log("The value of the subtraction of the numbers input is: " + value);
